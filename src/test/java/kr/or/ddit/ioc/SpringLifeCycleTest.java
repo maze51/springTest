392292525ed38531004daf31cb5d23458b5dbbc6
YC <mazexit@gmail.com>
2019-06-20 13:32:53 +0900

    spring ioc bean life cycle  - ApplicationContextAware 구현     : spring ioc 객체를 인자로 받을 수 있다  - <bean init-method="init">     : 개발자가 임의의 초기화 로직을 구성할 수 있는 메서드를 spring bean 객체 생성시 적용

---
+++ src/test/java/kr/or/ddit/ioc/SpringLifeCycleTest.java
@@ -0,0 +1,34 @@
+package kr.or.ddit.ioc;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.ioc.placeholder.DbInfo;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:kr/or/ddit/ioc/application-ioc-placeholder.xml")
+public class SpringLifeCycleTest {
+
+	@Resource(name="dbInfo")
+	private DbInfo dbInfo;
+	
+	@Test
+	public void lifeCycleTest() {
+		/***Given***/
+
+		/***When***/
+
+		/***Then***/
+		assertNotNull(dbInfo.getDriver());
+		assertEquals("oracle.jdbc.driver.OracleDriver", dbInfo.getDriver());
+		assertEquals("PC10", dbInfo.getUsername());
+		assertTrue(dbInfo.getUrl().equals("jdbc:oracle:thin:@localhost:1521:xe"));
+	}
+
+}
