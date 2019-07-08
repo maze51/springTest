a9843fcc33cc89a1fe9be32735e8adf8fec16a4d
YC <mazexit@gmail.com>
2019-07-08 19:11:38 +0900

    Spring batch  - 문자열 처리 배치 오류 수정

--- src/main/java/kr/or/ddit/config/spring/RootContext.java
+++ src/main/java/kr/or/ddit/config/spring/RootContext.java
@@ -10,12 +10,12 @@ import org.springframework.stereotype.Service;
 
 // kr/or/ddit/config/spring/root-context.xml을 자바 config로 변경
 // @Service, @Repository annotation을 대상으로 스캔
+@ImportResource({"classpath:kr/or/ddit/config/spring/application-scheduler.xml",
+"classpath:kr/or/ddit/config/spring/application-batch.xml"})
 @Configuration
 @ComponentScan(basePackages = "kr.or.ddit", useDefaultFilters = false,
-				includeFilters = {@Filter(type = FilterType.ANNOTATION, classes = {Service.class, Repository.class})})
+				includeFilters = @Filter(type = FilterType.ANNOTATION, classes = {Service.class, Repository.class}))
 //useDefaultFilters = false 하지 않으면 bean이 과다 생성됨 => 리소스 낭비
-@ImportResource({"classpath:kr/or/ddit/config/spring/application-scheduler.xml",
-				 "classpath:kr/or/ddit/config/spring/application-batch.xml"})
 public class RootContext {
 
 }
